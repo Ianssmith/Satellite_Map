661a6fd6f11d0b018aa66ed70f963c2089e75b88
ianssmith <ian.s.smith0317@gmail.com>
2016-04-09 02:49:03 -0400

    Further visualizing and analyzing data with python; Converting varibles to numeric form; Preliminary plotting of data to d3 primitives

--- Sat.js
+++ Sat.js
@@ -1,9 +1,59 @@
+function findMax(arr){
+  var max = arr[0];
+  for(i=0;i<arr.length;i++){
+    if(arr>max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+function findMin(arr){
+  var min = arr[0];
+  for(i=0;i<arr.length;i++){
+    if(arr<min){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+
 data = d3.csv("CSatellites.csv", function(data) {viz(data);});
- function viz(satData){
-  d3.select("body").selectAll("div.names")
-    .data(satData)
+
+
+ function viz(incomingData){
+
+  d3.select("body").selectAll("div.satellites")
+    .data(incomingData)
     .enter()
     .append("div")
-    .attr("class", "names")
+    .attr("class", "satellites")
     .html(function(d,i) {return d.name;});
-  }
+  
+
+var maxInclination = d3.max(incomingData, function(el) {return el.inclination;});
+var maxLongitude = d3.max(incomingData, function(el) {return el.longitude;});
+var minLongitude = d3.min(incomingData, function(el) {return el.longitude;});
+
+var radiusScale = d3.scale.linear().domain([200,330000]).range([5,300]);
+var colorScale = d3.scale.linear().domain([0,maxInclination]).range(["#000099", "#eeeeee"]);
+var longiScale = d3.scale.linear().domain([minLongitude, maxLongitude]).range([700,2000]);
+var inclinationScale = d3.scale.linear().domain([0, maxInclination]).range([50,1000]);
+
+d3.select("svg")
+  .selectAll("ellipse")
+  .data(incomingData)
+  .enter()
+  .append("ellipse")
+  .attr("rx", function(d) {return radiusScale(d.apogee_km);})
+  .attr("ry", function(d) {return radiusScale(d.perigee_km);})
+  .attr("cx", function(d) {return inclinationScale(d.inclination);})
+  .attr("cy", function(d) {return longiScale(d.longitude);})
+  .style("fill", function(d) {return colorScale(d.inclination);})
+  .style("stroke", "red")
+  .style("stroke-width", "0.5px")
+  .style("opacity", .35)
+
+
+}
